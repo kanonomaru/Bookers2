36d16cf92d855812db8070b6ff2ac2feb9983dd3
kanonomaru <kurono4maru5@gmail.com>
2022-06-01 12:35:25 +0000

    [Finish] 4-25

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'users/index'
   post 'books' => "books#create"
   resources :books, only: [:new, :index, :show, :destroy, :edit]
-  resources :users, only: [:show, :edit, :index]
+  resources :users, only: [:show, :edit, :index, :update]
   root to: "homes#top"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_27_003901) do
+ActiveRecord::Schema.define(version: 2022_06_01_121750) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
